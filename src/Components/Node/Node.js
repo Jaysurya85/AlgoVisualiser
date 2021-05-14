29dfbab1e0802b6481225339ebc5b8cf541ba316
Jaysurya <jaysuryaray1998@gmail.com>
2021-05-14 18:49:06 +0530

    just being safe

--- src/Components/Node/Node.js
+++ src/Components/Node/Node.js
@@ -5,10 +5,26 @@ const Node = (props)=>{
     let color = props.path?"path":props.search?"search":props.start?"start":props.end?"end":props.visited? "set":props.weight?"weight":"";
     let innerText = "";
     if(props.start){
-        innerText="S";
+        innerText=<svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="1em" 
+                        height="1em" 
+                        viewBox="0 0 16 16"><g fill="black">
+                            <path 
+                            d="M4.25 3l1.166-.624l8 5.333v1.248l-8 5.334l-1.166-.624V3zm1.5 1.401v7.864l5.898-3.932L5.75 4.401z"/>
+                            </g>
+                </svg>;
     }
     if(props.end){
-        innerText ="E";
+        innerText =<svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="1em" 
+                        height="1em" 
+                        viewBox="0 0 16 16">
+                            <g fill="black">
+                                <path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.713 3.31 4 3.655 4 4.308v7.384c0 .653.713.998 1.233.696L11.5 8.752V12a.5.5 0 0 0 1 0V4zM5 4.633L10.804 8L5 11.367V4.633z"/>
+                                </g>
+                                </svg>
     }
     if(props.weight){
         innerText = <svg 
